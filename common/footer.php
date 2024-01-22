d3c2d7bbc49f311eae4539d2b47296c2be8e4497
Salman <salmanmohammad@gmail.com>
2024-01-22 19:17:46 +0530

    folder structure updates

---
+++ common/footer.php
@@ -0,0 +1,12 @@
+ <!-- Footer -->
+ 
+ </div> <!-- /container -->  
+ <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+ <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <!-- The AJAX login script -->
+    <script src="js/login.js?id=12"></script>
+    <script src="js/create.js?id=12"></script>
+  </body>
+</html>
