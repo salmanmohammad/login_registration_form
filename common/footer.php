b6546bf7de3f68d97abb452b1f1cc3818677b8aa
Salman <salmanmohammad@gmail.com>
2024-01-23 22:23:55 +0530

    dashboard updated

--- common/footer.php
+++ common/footer.php
@@ -1,8 +1,8 @@
  <!-- Footer -->
  
  </div> <!-- /container -->  
- <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
- <script src="//code.jquery.com/jquery.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script type="text/javascript" src="js/bootstrap.js"></script>
     <!-- The AJAX login script -->
--- index.php
+++ index.php
@@ -4,32 +4,73 @@
   if(!isset($_SESSION['username'])){
     header("location:login.php");
   }
-  else{
-    if($_SESSION['profile_image'] != "")
-    {
-      $img = $_SESSION['profile_image'];
-      echo "<img src='uploads/$img' style='width:100px'/>";
-    }
-  echo "<br>Welcome  ".$_SESSION['username'];
-}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
+    <link rel="icon" type="image/x-icon" href="images/favicon.png">
+  
     <link href="css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="css/main.css" rel="stylesheet" media="screen">
+    <link href="css/dashboard.css?id=12" rel="stylesheet" media="screen">
   </head>
   <body>
-    <div class="container">
-      <div class="form-signin">
-        <div class="alert alert-success">You have been <strong>successfully logged in</strong>.</div>
-        <a href="logout.php" class="btn btn-default btn-lg btn-block">Logout</a>
-      </div>
-    </div> <!-- /container -->
-  </body>
-</html>
+    <div id="throbber" style="display:none; min-height:120px;"></div>
+    <div id="noty-holder"></div>
+    <div id="wrapper">
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+                User Dashboard
+            </a>
+        </div>
+        <!-- Top Menu Items -->
+        <ul class="nav navbar-right top-nav">
+            <li><?php echo "<img src='uploads/".$_SESSION['profile_image']."' style='width:100px'/>"; ?>
+            </li>            
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['username']; ?> <b class="fa fa-angle-down"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="./logout.php"><i class="fa fa-fw fa-power-off"></i> Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav side-nav">
+                
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </nav>
+
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row" id="main" >
+                <div class="col-sm-12 col-md-12 well" id="content">
+                    <h1>Welcome <?php echo $_SESSION['username']; ?>!</h1>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+</div><!-- /#wrapper -->
+<script src="//code.jquery.com/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script src="js/dashboard.js?id=12"></script>
+</body>
+</html> 
